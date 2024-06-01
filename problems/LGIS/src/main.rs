54674163d9d580db70cf8e14207924a8a3b72dac
Brett Bowman <bnbowman@Bretts-MacBook-Pro.local>
2024-06-01 13:46:25 -0700

    Finish LGIS solution

--- problems/LGIS/src/main.rs
+++ problems/LGIS/src/main.rs
@@ -2,6 +2,104 @@ use std::env;
 use std::fs;
 use std::io::{BufRead, BufReader};
 
+fn get_subsequence(
+    vec_x: &Vec<usize>,
+    l: usize,
+    first_index: usize,
+    vec_p: Vec<usize>,
+) -> Vec<usize> {
+    let mut vec_s = vec![0usize; l];
+    let mut k = first_index;
+    for i in (0..l).rev() {
+        vec_s[i] = vec_x[k] as usize;
+        k = vec_p[k] as usize;
+    }
+
+    return vec_s;
+}
+
+fn get_midpoint(lo: usize, hi: usize) -> usize {
+    return (lo as f32 + ((hi - lo) as f32 / 2.0).floor()) as usize;
+}
+
+fn longest_increasing_subsequence(vec_x: &Vec<usize>) -> Vec<usize> {
+    let n = vec_x.len();
+    let mut vec_m = vec![0usize; n + 1];
+    let mut vec_p = vec![0usize; n];
+    let mut l = 0usize;
+    for i in 0..n {
+        // Binary search for the smallest positive l ≤ L
+        // such that X[M[l]] > X[i]
+        let mut lo = 1;
+        let mut hi = l + 1;
+        while lo < hi {
+            let mid = get_midpoint(lo, hi); // lo <= mid < hi
+            if vec_x[vec_m[mid as usize] as usize] >= vec_x[i] {
+                hi = mid;
+            } else {
+                // if X[M[mid]] < X[i]
+                lo = mid + 1;
+            }
+        }
+
+        // After searching, lo == hi is 1 greater than the
+        // length of the longest prefix of X[i]
+        let new_l = lo;
+
+        // The predecessor of X[i] is the last index of
+        // the subsequence of length newL-1
+        vec_p[i] = vec_m[new_l - 1];
+        vec_m[new_l] = i;
+
+        if new_l > l {
+            // If we found a subsequence longer than any we've
+            // found yet, update L
+            l = new_l;
+        }
+    }
+
+    return get_subsequence(vec_x, l, vec_m[l], vec_p);
+}
+
+fn longest_decreasing_subsequence(vec_x: &Vec<usize>) -> Vec<usize> {
+    let n = vec_x.len();
+    let mut vec_m = vec![0usize; n + 1];
+    let mut vec_p = vec![0usize; n];
+    let mut l = 0usize;
+    for i in 0..n {
+        // Binary search for the smallest positive l ≤ L
+        // such that X[M[l]] > X[i]
+        let mut lo = 1;
+        let mut hi = l + 1;
+        while lo < hi {
+            let mid = get_midpoint(lo, hi); // lo <= mid < hi
+            if vec_x[vec_m[mid as usize] as usize] <= vec_x[i] {
+                hi = mid;
+            } else {
+                // if X[M[mid]] < X[i]
+                lo = mid + 1;
+            }
+        }
+
+        // After searching, lo == hi is 1 greater than the
+        // length of the longest prefix of X[i]
+        let new_l = lo;
+
+        // The predecessor of X[i] is the last index of
+        // the subsequence of length newL-1
+        vec_p[i] = vec_m[new_l - 1];
+        vec_m[new_l] = i;
+
+        if new_l > l {
+            // If we found a subsequence longer than any we've
+            // found yet, update L
+            l = new_l;
+        }
+    }
+
+    return get_subsequence(vec_x, l, vec_m[l], vec_p);
+}
+
 fn main() -> Result<(), std::io::Error> {
     let args: Vec<String> = env::args().collect();
     let filepath: String = args[1].clone();
@@ -10,10 +108,8 @@ fn main() -> Result<(), std::io::Error> {
     let mut reader = BufReader::new(file);
     let mut line = String::new();
 
-    let mut N: usize = 0;
-    let mut X: Vec<i32> = Vec::<i32>::new();
-    let mut P: Vec<i32> = Vec::<i32>::new();
-    let mut M: Vec<i32> = Vec::<i32>::new();
+    let mut n: usize = 0;
+    let mut vec_x: Vec<usize> = Vec::<usize>::new();
     loop {
         let bytes_read = reader.read_line(&mut line)?;
         if bytes_read == 0 {
@@ -22,64 +118,34 @@ fn main() -> Result<(), std::io::Error> {
         let l = line.trim_end().len();
         line.truncate(l);
 
-        if N == 0 {
-            N = line.parse::<usize>().unwrap();
-            M = vec![0; N+1];
-            P = vec![-1; N];
+        if n == 0 {
+            n = line.parse::<usize>().unwrap();
         } else {
-            X = line
+            vec_x = line
                 .split_whitespace()
-                .map(|n| n.parse::<i32>().unwrap())
-                .collect::<Vec<i32>>();
+                .map(|n| n.parse::<usize>().unwrap())
+                .collect::<Vec<usize>>();
         }
 
         line.clear();
     }
 
-    let mut L = 0usize;
-    for i in (0..N) { //N-1 included
-        // Binary search for the smallest positive l ≤ L
-        // such that X[M[l]] > X[i]
-        let mut lo = 1;
-        let mut hi = L + 1;
-        while lo < hi {
-            let mid = (lo as f32 + ((hi-lo) as f32/2.0).floor()) as usize; // lo <= mid < hi
-            if X[M[mid as usize] as usize] >= X[i] {
-                hi = mid;
-            } else { // if X[M[mid]] < X[i]
-                lo = mid + 1;
-            }
-        }
+    let vec_i = longest_increasing_subsequence(&vec_x);
+    let vec_d = longest_decreasing_subsequence(&vec_x);
 
-        // After searching, lo == hi is 1 greater than the
-        // length of the longest prefix of X[i]
-        let newL = lo;
+    let i = vec_i
+        .iter()
+        .map(|i| i.to_string())
+        .collect::<Vec<String>>()
+        .join(" ");
+    let d = vec_d
+        .iter()
+        .map(|d| d.to_string())
+        .collect::<Vec<String>>()
+        .join(" ");
 
-        // The predecessor of X[i] is the last index of 
-        // the subsequence of length newL-1
-        P[i] = M[newL-1];
-        M[newL] = i as i32;
-        
-        if newL > L {
-            // If we found a subsequence longer than any we've
-            // found yet, update L
-            L = newL;
-        }
-    }
-    
-    println!("{}", N);
-    println!("{:?}", X);
-    println!("{:?}", M);
-    println!("{:?}", P);
+    println!("{}", i);
+    println!("{}", d);
 
-    let mut S = vec![0i32; L];
-    let mut k = M[L] as usize;
-    for j in (0..L).rev() { //0 included
-        S[j] = X[k];
-        k = P[k] as usize;
-    }
-
-    println!("{:?}", S);
-    
     Ok(())
 }
